121bf85d50bfdabfbe43d37dff49644ddf601cb6
GracielaMoreno <chelinesmoreno@hotmail.com>
2017-01-20 11:43:01 -0500

    Update Aplicacion.java

--- Aplicacion.java
+++ Aplicacion.java
@@ -1,38 +1,68 @@
+package epn;
+
+import javax.swing.JOptionPane;
 
 public class Aplicacion {
 
 	public static void main(String[] args) {
 
 		ListaDoble a=new ListaDoble();
+	
+		
+		System.out.println("<<-- Lista -->>");
+		
+		System.out.println("Impresion Normal"); 
+
+		a.listarN();
+		
+		//nodo nuevo al inicio
+		System.out.println("Impresion la lista con los nodos agregados al inicio"); 
+
 		a.AgregarInicio(15);
 		a.AgregarInicio(16);
 		a.AgregarInicio(17);
 		a.AgregarInicio(18);
 		a.AgregarInicio(19);
+		a.listarN();
+		
+		System.out.println("Impresion la lista con los nodos agregados al fin"); 
 		a.AgregarFin(1);
 		a.AgregarFin(2);
 		a.AgregarFin(3);
-		System.out.println("<<-- Lista -->>");
 		a.listarN();
-		System.out.println("Impresion Normal"); 
-
-		a.listarN();
+		//imprimir lista inversa
 		System.out.println("Impresion Inversa"); 
 		a.listarIN();
+		//elemento buscado 
+		System.out.println("buscar por referencia el elemento 20"); 
+		System.out.println(a.buscar(20));
 
-		System.out.println("buscar por referencia el 15"); 
+		System.out.println("buscar por referencia el elemento 15"); 
 		System.out.println(a.buscar(15));
-
-		System.out.println("\nObtener el valor del nodo en la posicion 1");
-		System.out.println(a.buscarP(1));
-		System.out.println("borrar el elemento 19"); 
-		a.borrarPorDato(19);
+		
+		//borrar el elemnto buscado
+		System.out.println("borrar el elemento 15"); 
+		a.borrarPorDato(15);
 		a.listarN();
-		System.out.println("\nElimina el nodo en la posición 7");
-		a.removerPorPosicion(7);        
-		a.listarN();
-		System.out.println("borrar todo"); 
-		a.borrar();
-		a.listarN();
+		
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//System.out.println("\nObtener el valor del nodo en la posicion 1");
+//System.out.println(a.buscarP(1));
+
+//System.out.println("borrar todo"); 
+//a.borrar();
+//a.listarN();
+//imprime la lista
