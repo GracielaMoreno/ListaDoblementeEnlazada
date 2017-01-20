719266bc36e2f8af3d205ab9f7c4dc8c4b3cfc5b
AlexaVane <alexavanemacas321@gmail.com>
2017-01-20 11:09:41 -0500

    Update NodoDoble.java

--- NodoDoble.java
+++ NodoDoble.java
@@ -1,17 +1,40 @@
-package epn;
 
 public class NodoDoble {
 
-
+　
 	int dato;
 	NodoDoble next;
 	NodoDoble ant;
+	
+	
 	NodoDoble(int a){
 		this.dato=a;
 		this.next=null;
 		this.ant=null;
 	}
+	public int getDato() {
+		return dato;
+	}
+	public void setDato(int dato) {
+		this.dato = dato;
+	}
+	public NodoDoble getNext() {
+		return next;
+	}
+	public void setNext(NodoDoble next) {
+		this.next = next;
+	}
+	public NodoDoble getAnt() {
+		return ant;
+	}
+	public void setAnt(NodoDoble ant) {
+		this.ant = ant;
+	}
+	public int getDato1() {
+		// TODO Auto-generated method stub
+		return dato;
+	}
 
-
+　
 	//buscar borrar
 }
